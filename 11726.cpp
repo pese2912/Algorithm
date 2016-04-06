cdead7d0d5a12aa68b43d8143119100907a2a167
pese2912 <pese2912@naver.com>
2016-04-06 22:27:59 +0900

    Create 11726.cpp

---
+++ 11726.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int arr[1001] = { 0, };
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	arr[0] = 0, arr[1] = 1, arr[2] = 2;
+
+	for (int i = 3; i <= n; i++)
+	{
+		arr[i] = arr[i - 1] + arr[i - 2];
+		arr[i] %= 10007;
+	}
+
+	cout << arr[n]%10007 << '\n';
+	
+	return 0;
+}
